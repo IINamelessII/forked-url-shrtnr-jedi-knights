a59b233cb093d7af2712434642dd9675bc18fc9e
RedBullDogE <gsb06092000@gmail.com>
2021-03-16 22:19:23 +0200

    Pair programming step #1: implement basic flow

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -1,14 +1,52 @@
 from locust import HttpUser, task, between
+import logging
+import random
+
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
+    @task(10)
+    def redirect(self):
+        self.client.get(
+            "/r/" + random.choice(self.urls)['alias']
+        )
+
     @task
-    def hello_world(self):
-        self.client.get("/r/palevo")
+    def shorten(self):
+        self.client.post(
+            "/urls/shorten",
+            headers={
+                'Authorization': self.token
+            },
+            json={
+                "uri": "https://google.com"
+            }
+        )
 
     def on_start(self):
-        reponse = self.client.post(
-          "/users/signin", json={"email": "aaa@example.com", "password": "password1"}
+        self.signup()
+        self.login()
+        self.get_urls()
+
+    def signup(self):
+        self.client.post("/users/signup", json={
+            "email": "drew@ex.com",
+            "password": "password1"
+        })
+
+    def login(self):
+        with self.client.post("/users/signin", json={
+            "email": "drew@ex.com",
+            "password": "password1"
+        }) as response:
+            self.token = response.json().get('token')
+
+    def get_urls(self):
+        response = self.client.get(
+            "/urls",
+            headers={
+                'Authorization': self.token
+            }
         )
-        print(reponse)
+        self.urls = response.json().get('urls')
